f12e619f39c74b6129fa74964489d0e7d4392982
Muhammad Lukman Toro <torolukman355@gmail.com>
2025-11-29 09:26:56 +0700

    lorem ipsum

---
+++ jepin.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    printf ("lorem ipsum dolor sit amet\n");
+    if (1 + 1 == 2) {
+        printf ("consectetur adipiscing elit\n");
+    }else{
+        printf ("sed do eiusmod tempor incididunt\n");
+    }
+    return 0;
+}
